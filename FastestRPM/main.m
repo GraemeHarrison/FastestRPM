a3919ff1b1433d54bcc7e64b87516fb75a3ae4d1
Graeme Harrison <gharrison365@gmail.com>
2016-01-21 11:13:35 -0500

    Initial commit

---
+++ FastestRPM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FastestRPM
+//
+//  Created by Graeme Harrison on 2016-01-21.
+//  Copyright © 2016 Graeme Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
